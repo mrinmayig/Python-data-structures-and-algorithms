85aa454c03e9f038860a1db981fcdaee2bb257c6
Mrinmayi Gavali <mrinmayigavali2@gmail.com>
2019-02-25 23:03:53 -0800

    queue

---
+++ queue.py
@@ -0,0 +1,14 @@
+#assume that right is the front
+class Queue:
+    def __init__(self):
+        self.items = []
+    def isEmpty(self):
+        return self.items == []
+    def enqueue(self,item):
+        self.items.insert(0,item)
+    def dequeue(self):
+        return self.items.pop()
+    def size(self):
+        return len(self.items)
+    def view(self):
+        return self.items
