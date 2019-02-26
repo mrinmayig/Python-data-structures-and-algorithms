036439f101c3ffbd70898eef06fc43e9d1d3ae8a
Mrinmayi Gavali <mrinmayigavali2@gmail.com>
2019-02-26 00:27:53 -0800

    binary-search-iterative-recursive

---
+++ binarysearch.py
@@ -0,0 +1,25 @@
+def binary_search_iterative(data, target):
+    low = 0
+    high = len(data) - 1
+    while low <= high:
+        mid = (low + high)//2
+        if target == data[mid]:
+            return True
+        elif target < data[mid]:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return False
+
+
+def binary_search_recursive(data, target, low, high):
+    if low>high:
+        return False
+    else:
+        mid = (low+high)//2
+        if target == data[mid]:
+            return True
+        elif target < data[mid]:
+            return binary_search_recursive(data, target, low, mid-1)
+        else:
+            return binary_search_recursive(data, target, mid+1, high)
