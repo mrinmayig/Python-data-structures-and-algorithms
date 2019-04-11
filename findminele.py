626de659237a55c652ca873559331da0fbabec35
mrinmayig <mrinmayigavali2@gmail.com>
2019-04-10 22:43:56 -0700

    min_element_dynamic

---
+++ findminele.py
@@ -0,0 +1,10 @@
+#Given an array of length N find the minimum element in the array
+
+def maxElem(arr):
+    curr_num=0
+    min_num=arr[0]
+    for i in range(1,len(arr)):
+        curr_num=arr[i]
+        if curr_num < min_num:
+            min_num=curr_num
+    return min_num
