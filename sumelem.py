fd66c46119929ba3294812b48d9aef4413429696
mrinmayig <mrinmayigavali2@gmail.com>
2019-04-10 23:11:00 -0700

    sumelemnew

--- sumelem.py
+++ sumelem.py
@@ -1,8 +1,11 @@
 #Given an array of size N return the sum of all its elements
 
 def sumofele(arr):
-    curr_sum=arr[0]
-    for i in range(1,len(arr)):
-        curr_sum = curr_sum + arr[i]
-    return curr_sum
-    
+	
+    if len(arr)>0:
+        curr_sum=arr[0]
+        for i in range(1,len(arr)):
+            curr_sum = curr_sum + arr[i]
+        return curr_sum
+    else:
+        return 0
