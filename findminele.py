3e67992bb3b7892d33077e74bf8b8c7d53ea89d9
mrinmayig <mrinmayigavali2@gmail.com>
2019-04-10 22:52:35 -0700

    minmax

--- findminele.py
+++ findminele.py
@@ -8,3 +8,15 @@ def minElem(arr):
         if curr_num < min_num:
             min_num=curr_num
     return min_num
+
+
+#Given an array of length N find the maximum element in the array
+
+def maxElem(arr):
+    curr_num=0
+    max_num=arr[0]
+    for i in range(1,len(arr)):
+        curr_num=arr[i]
+        if curr_num > max_num:
+            max_num=curr_num
+    return max_num
