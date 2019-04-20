49e595ab250a9f915ba4a987cb5565718f147e3b
mrinmayig <mrinmayigavali2@gmail.com>
2019-04-20 14:47:28 -0700

    selsort

---
+++ selectionsort.py
@@ -0,0 +1,13 @@
+#Selection Sort
+
+def SelectionSort(arr):
+    if len(arr)<=1:
+        return arr
+    else:
+        for i in range(len(arr)):
+            min_index=i
+            for j in range(i+1,len(arr)):
+                if arr[j]<arr[min_index]:
+                    min_index=j
+            arr[i], arr[min_index]=arr[min_index], arr[i]
+        return arr
